d8bb3031d2a8980f1b9f78fb4a9a78c6788e51f9
Mikio Takeuchi <mtake@jp.ibm.com>
2025-11-04 21:59:11 -0500

    update script

--- get_responses_mtake.sh
+++ get_responses_mtake.sh
@@ -41,8 +41,8 @@ MODELS=()
 #MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft_interp")  # v0.2
 #MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft_interp_0.6" "granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft_interp_0.7")  # v0.2
 #MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_osft_interp")  # v0.2
-#MODELS+=("ibm-granite/granite-4.0-h-small")  # 1 GPU(vllm 0.11.0)
-MODELS+=("granite-4.0-h-small_teigaku-genzei-ibm-v6_sft_interp" "granite-4.0-h-small_teigaku-genzei-ibm-v6_sft")  # 1 GPU(vllm 0.11.0)
+#MODELS+=("ibm-granite/granite-4.0-h-small")  # 2 GPUs (vllm 0.9.2) (or 1 GPU (vllm 0.11.0) ?)
+MODELS+=("granite-4.0-h-small_teigaku-genzei-ibm-v6_sft_interp" "granite-4.0-h-small_teigaku-genzei-ibm-v6_sft")  # 2 GPUs (vllm 0.9.2) (or 1 GPU (vllm 0.11.0) ?)
 
 ENV=""
 #ENV="TOKENIZERS_PARALLELISM=false ${ENV}"
