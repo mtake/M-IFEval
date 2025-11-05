1503aa4d9addcf9b4ac1e9b750488632d8560c88
Mikio Takeuchi <mtake@jp.ibm.com>
2025-11-04 21:25:23 -0500

    add granite-4.0-h-small_teigaku-genzei-ibm-v6_sft_interp

--- run_mtake.sh
+++ run_mtake.sh
@@ -38,7 +38,8 @@ MODELS=()
 #MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft_interp")  # v0.2
 #MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft_interp_0.6" "granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_sft_interp_0.7")  # v0.2
 #MODELS+=("granite-3.3-8b-instruct_teigaku-genzei-ibm-v6_osft_interp")  # v0.2
-MODELS+=("ibm-granite/granite-4.0-h-small")
+#MODELS+=("ibm-granite/granite-4.0-h-small")
+MODELS+=("granite-4.0-h-small_teigaku-genzei-ibm-v6_sft_interp")
 
 # @@@ahoaho XXX
 #INPUT_DIR=./data
